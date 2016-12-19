1143b22a330b49184aad9a36481ac11688a730bc
hahakumquat <michael.zh.ge@gmail.com>
2016-12-19 13:47:32 -0800

    basic material complete system

--- site/js/main.js
+++ site/js/main.js
@@ -3,70 +3,120 @@ var _;
 
 $(document).ready(function() {
     var loader = new THREE.TextureLoader();
-    loader.load("img/particle.jpg", function(texture) {
-	init(texture);
-	animate();
+    loader.load("img/particle.png", function(texture) {
+	_ = {};
+	_.texture = texture;
+	init();
+	animate();	
+    });
+
+    // resize screen
+    window.addEventListener("resize", function() {
+	_.width = window.innerWidth;
+	_.height = window.innerHeight;
+	_.aspect = _.width / _.height;
+
+	_.camera.aspect = _.aspect;
+	_.camera.updateProjectionMatrix();
+
+	_.renderer.setSize(_.width, _.height);
+    });
+
+    // Leaving page bug
+    $(window).blur(function(){
+	_.particleSystem.time.start();
+    });
+    $(window).focus(function(){
+	_.particleSystem.time.stop();
     });
 });
-
 /***************************************************************/
 /*********************INIT FUNCTIONS****************************/
 /***************************************************************/
 
 function init(texture) {
-	_ = {};
-	initParameters(_);
-	initObjects(_, texture);
+	initParameters();
+	initObjects();
 }
 
 // returns global parameters
-function initParameters(params) {
+function initParameters() {
 
-    params.container = document.getElementById("container");
-    params.width = window.innerWidth;
-    params.height = window.innerHeight;
-    params.view_angle = 45;
-    params.aspect = params.width / params.height;
-    params.near = 0.1;
-    params.far = 1000;
+    _.container = document.getElementById("container");
+    _.width = window.innerWidth;
+    _.height = window.innerHeight;
+    _.view_angle = 45;
+    _.aspect = _.width / _.height;
+    _.near = 0.1;
+    _.far = 2000;
+
+    // particle parameters
+    _.canvasParticle = false;
+    _.maxParticles = 10000;
+    _.size = 1.;
+    _.lifetime = 5.;
+
+    // physics parameters
+    _.physics = true;
+    _.gravity = new THREE.Vector3(0, -10, 0);
+    _.wind = new THREE.Vector3(0, 0, 0);
 }
 
 // returns scene, camera, controls, renderer, particlesystem
-function initObjects(params, texture) {
+function initObjects() {
 
-    params.scene = new THREE.Scene();
+    _.scene = new THREE.Scene();
     
-    params.camera= new THREE.PerspectiveCamera(
-	params.view_angle,
-	params.aspect,
-	params.near,
-	params.far);    
+    _.camera= new THREE.PerspectiveCamera(
+	_.view_angle,
+	_.aspect,
+	_.near,
+	_.far);    
 
-    params.renderer = new THREE.WebGLRenderer();
-    params.renderer.setSize(params.width, params.height);
-    params.container.appendChild(params.renderer.domElement);
-
+    _.renderer = new THREE.WebGLRenderer();
+    _.renderer.setSize(_.width, _.height);
+    _.container.appendChild(_.renderer.domElement);
+    
     // orbit controls
-    params.controls = new THREE.OrbitControls(params.camera, params.renderer.domElement);
-    params.controls.enableDamping = true;
-    params.controls.dampingFactor = 0.25;
-    params.controls.enableZoom = true;
-    // params.controls.autoRotate = true;
-
-    // var sphere = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshBasicMaterial({color: "white"}));
-    // params.scene.add(sphere);
+    _.controls = new THREE.OrbitControls(_.camera, _.renderer.domElement);
+    _.controls.enableDamping = true;
+    _.controls.rotateSpeed = 0.2;
+    _.controls.zoomSpeed = 0.5;
+    _.controls.dampingFactor = 0.25;
+    _.controls.enableZoom = true;
     
-    params.camera.position.z = 300;
-    params.scene.add(params.camera);
-
-    particleParams = {
-    	maxParticles: 5000,
-	size: 1.,
-    };
+    // aesthetic background
+    initBackground();
     
+    _.camera.position.z = 100;
+    _.camera.position.y = 50;
+    _.scene.add(_.camera);
 
-    params.particleSystem = new ParticleSystem(particleParams, texture);
-    params.scene.add(params.particleSystem.points);
+    // overwrite jpg texture with canvas texture if desired
+    if (_.canvasParticle) {	
+	_.canvas = document.createElement("canvas");
+	_.canvas.width = 256;
+	_.canvas.height = 256;
+	_.ctx = _.canvas.getContext("2d");
+	
+	_.texture = new THREE.Texture(_.canvas);
+	document.body.appendChild(_.canvas);
+    }
+    
+    _.particleSystem = new ParticleSystem(_);
+    _.scene.add(_.particleSystem.points);
+}
+
+function initBackground() {
+    var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+    light.position.set(0, 70, -200);
+    var sphere = new THREE.Mesh(new THREE.SphereGeometry(1000, 50, 50),
+				new THREE.MeshLambertMaterial({
+        color: 0xaaaaaa,
+	side: THREE.BackSide,
+    }));
+    _.scene.add(light);
+    _.scene.add(sphere);
 }
 
 /***************************************************************/
@@ -75,32 +125,21 @@ function initObjects(params, texture) {
 
 function animate() {
     _.controls.update();
+    // drawCanvas();
     _.particleSystem.update();
     _.renderer.render(_.scene, _.camera);
     requestAnimationFrame(animate);
 }
 
-/***************************************************************/
-/**************************MISCELLANEOUS************************/
-/***************************************************************/
-
-// resize screen
-window.addEventListener("resize", function() {
-    _.width = window.innerWidth;
-    _.height = window.innerHeight;
-    _.aspect = _.width / _.height;
-
-    _.camera.aspect = _.aspect;
-    _.camera.updateProjectionMatrix();
-
-    _.renderer.setSize(_.width, _.height);
-    requestAnimationFrame(animate);
-});
-
-// exploding particles bug with timer
-$(window).blur(function(){
-    _.particleSystem.time.start();
-});
-$(window).focus(function(){
-    _.particleSystem.time.stop();
-});
+function drawCanvas() {
+    _.ctx.fillRect(0, 0, 256, 256);
+    var w = _.canvas.width / 2;
+    var h = _.canvas.height / 2;
+    var r = w * .4;
+    _.ctx.arc(-w, -h, r, 0, 2  * Math.PI);
+    _.ctx.shadowColor = "#010890";
+    _.ctx.shadowBlur = 200;
+    _.ctx.shadowOffsetX = w * 2;
+    _.ctx.shadowOffsetY = h * 2;
+    _.ctx.fill();
+}
--- site/js/particleSystem.js
+++ site/js/particleSystem.js
@@ -1,63 +1,136 @@
 // particle system class
-ParticleSystem = function(params, particleTex) {
+ParticleSystem = function(params) {
 
     var self = this;
+    // timer 
     self.time = new THREE.Clock(true);
-    self.position = params.position || new THREE.Vector3(0, 0, 0);
-    self.maxParticles = params.maxParticles || 1000;
-    self.lifetime = params.lifetime || 5.0;
-    self.color = params.color || new THREE.Color(0xFFFFFF);
-    self.size = params.size || 1.0;
-    self.blending = params.blending || THREE.AdditiveBlending;
-    self.transparent = params.transparent || true;
+    
+    // initial position
+    self.position = params.position || new THREE.Vector3(0, -30, 0);
 
-    // // geometry object that holds buffers
-    var particleGeometry = new THREE.Geometry();
+    // particle rate
+    self.maxParticles = params.maxParticles || 1000;
+
+    // how long a particle lives
+    self.lifetime = params.lifetime || 5.0;
+
+    // how big a particle is
+    self.size = params.size || 1.0;
+
+    // newtonian physics
+    self.physics = params.physics || false;
+    self.gravity = params.gravity || new THREE.Vector3();
+    
+    self.wind = params.wind || new THREE.Vector3();
+
+    var particleMaterial = new THREE.PointsMaterial({
+    	color: 0xffffff,
+    	size: self.size,
+    	blending: THREE.AdditiveBlending,
+    	map: _.texture,
+    	transparent: true,
+    	alphaTest: 0.5,
+    	depthTest : false,
+    	depthWrite : false
+    });
+
+    var particleGeometry = new THREE.BufferGeometry();
+    // stores position of particle
+    var positionBuffer = new Float32Array(self.maxParticles * 3);
+    // stores initial velocity of particle
+    var velocityBuffer = new Float32Array(self.maxParticles * 3);
+    // initial life, life remaining
+    var lifeBuffer = new Float32Array(self.maxParticles * 2);
 
     for (var k = 0; k < self.maxParticles; k++) {
-	particleGeometry.vertices.push(new THREE.Vector3(0, Math.random(), Math.random()));
+	positionBuffer[k * 3 + 0] = self.position.x;
+	positionBuffer[k * 3 + 1] = self.position.y;
+	positionBuffer[k * 3 + 2] = self.position.z;
+
+	var theta = (Math.random()) * 2 * Math.PI;
+	var phi = (Math.random()) * Math.PI / 2;
+	velocityBuffer[k * 3 + 0] = Math.cos(theta) * 20;	
+	velocityBuffer[k * 3 + 2] = Math.sin(theta) * 20;
+	velocityBuffer[k * 3 + 1] = Math.tan(phi) * 20;
+	lifeBuffer[k * 2 + 0] = Math.random() * self.lifetime;
+	lifeBuffer[k * 2 + 1] = lifeBuffer[k * 2 + 0];
     }
+
+    var positionAttribute = new THREE.BufferAttribute(positionBuffer, 3);
+    positionAttribute.setDynamic(true);
+    particleGeometry.addAttribute("position", positionAttribute);
+
+    var velocityAttribute = new THREE.BufferAttribute(velocityBuffer, 1);
+    velocityAttribute.setDynamic(false);
+    particleGeometry.addAttribute("velocity", velocityAttribute);
     
-    var particleMaterial = new THREE.PointsMaterial({
-	color: 0xffffff,
-	size: self.size,
-	blending: self.blending,
-	map: particleTex,
-	transparent: self.transparent,
-	alphaTest: 0.5,
-	depthTest : false,
-	depthWrite : false
-    });
+    var lifeAttribute = new THREE.BufferAttribute(lifeBuffer, 2);
+    lifeAttribute.setDynamic(true);
+    particleGeometry.addAttribute("life", lifeAttribute);
     
     var particlePoints = new THREE.Points(particleGeometry, particleMaterial);
     
-    for (var k = 0; k < self.maxParticles; k++) {
-	
-	particlePoints.geometry.vertices[k].velocity = new THREE.Vector3(
-	    Math.random() / 2, Math.random() / 2, Math.random() / 2);
-	
-	particlePoints.geometry.vertices[k].life = self.lifetime - (k * self.lifetime / self.maxParticles);
-    }
     self.points = particlePoints;
 }
 
 ParticleSystem.prototype.update = function() {
     var self = this;
     var delta = self.time.getDelta();
+
+    var positions = self.points.geometry.attributes.position.array;
+    var velocities = self.points.geometry.attributes.velocity.array;
+    var lives = self.points.geometry.attributes.life.array;
+    
     for (var k = 0; k < self.maxParticles; k++) {
-	if (self.points.geometry.vertices[k].life - delta < 0) {
-	    self.points.geometry.vertices[k].life = self.lifetime;
-	    self.points.geometry.vertices[k].x = self.position.x;
-	    self.points.geometry.vertices[k].y = self.position.y;
-	    self.points.geometry.vertices[k].z = self.position.z;
-	}
-	else {
-	    self.points.geometry.vertices[k].life -= delta;
-	    var velocity = self.points.geometry.vertices[k].velocity;
-	    self.points.geometry.vertices[k].x += velocity.x;
-	    self.points.geometry.vertices[k].y += velocity.y;
-	    self.points.geometry.vertices[k].z += velocity.z;
-	}
+    	if (lives[k * 2 + 1] - delta < 0) {
+	    lives[k * 2 + 0] = Math.random() * self.lifetime;
+	    lives[k * 2 + 1] = lives[k * 2 + 0];
+    	    positions[k * 3 + 0] = self.position.x;
+    	    positions[k * 3 + 1] = self.position.y;
+    	    positions[k * 3 + 2] = self.position.z;
+    	}
+    	else {
+	    lives[k * 2 + 1] -= delta;	    
+	    var v0 = new THREE.Vector3(velocities[k * 3 + 0],
+				       velocities[k * 3 + 1],
+				       velocities[k * 3 + 2]);
+    	    var s0 = new THREE.Vector3(positions[k * 3 + 0],
+    				       positions[k * 3 + 1],
+    				       positions[k * 3 + 2]);
+
+	    if (self.physics) {
+		physicalMove(v0, s0, lives, k);
+	    }
+	    else {
+		basicMove(v0, k);
+	    }
+    	}
     }
-    self.points.geometry.verticesNeedUpdate = true;
+    self.points.geometry.attributes.position.needsUpdate = true;
+    self.points.geometry.attributes.life.needsUpdate = true;
+    _.texture.needsUpdate = true;
+    _.renderer.render(_.scene, _.camera);
+}
+
+function basicMove(v0, k) {
+    var self = _.particleSystem;
+    var positions = self.points.geometry.attributes.position.array;
+    var i = k * 3;
+    positions[i + 0] += v0.x / 100;
+    positions[i + 1] += v0.y / 100;
+    positions[i + 2] += v0.z / 100;
+}
+
+function physicalMove(v0, s0, lives, k) {
+    var self = _.particleSystem;
+    var positions = self.points.geometry.attributes.position.array;
+    var velocities = self.points.geometry.attributes.velocity.array;
+    var t = lives[k * 2 + 0] - lives[k * 2 + 1];
+    var i = k * 3;
+    var newX = self.position.x + self.wind.x * t + velocities[i + 0] * t + 0.5 * self.gravity.x * t * t;
+    var newY = self.position.y + self.wind.y * t + velocities[i + 1] * t + 0.5 * self.gravity.y * t * t;
+    var newZ = self.position.z + self.wind.z * t + velocities[i + 2] * t + 0.5 * self.gravity.z * t * t;
+    positions[k * 3 + 0] = newX;
+    positions[k * 3 + 1] = newY;
+    positions[k * 3 + 2] = newZ;
 }
